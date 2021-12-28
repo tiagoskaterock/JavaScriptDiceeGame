dfb082d1e8a3960d22a0c02356bac0d4c31c150e
tiago <tiagolemespalhano@gmail.com>
2021-12-28 19:13:19 -0300

    changing title and showing the Winner

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 // changing images
-document.querySelector("#img1").setAttribute('src', 'images/dice' + randomNumber1 + '.png');;
-document.querySelector("#img2").setAttribute('src', 'images/dice' + randomNumber2 + '.png');;
+document.querySelector("#img1").setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+document.querySelector("#img2").setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+
+// changing title and showing the Winner
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("#title").innerText = 'Player 1 Wins!!!';
+}
+else if(randomNumber2 > randomNumber1) {
+    document.querySelector("#title").innerText = 'Player 2 Wins!!!';
+}
+else {
+    document.querySelector("#title").innerText = 'Draw Game...';
+}
+
