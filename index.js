d99e28fa187501371435f2106d5248f3d2dab876
tiago <tiagolemespalhano@gmail.com>
2021-12-28 18:46:00 -0300

    Changing images of the dice

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-alert(randomNumber1)
-alert(randomNumber2)
+// changing images
+document.querySelector("#img1").setAttribute('src', 'images/dice' + randomNumber1 + '.png');;
+document.querySelector("#img2").setAttribute('src', 'images/dice' + randomNumber2 + '.png');;
