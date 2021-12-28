827de2bbe8464e6438671c2691edd58f32369a96
tiago <tiagolemespalhano@gmail.com>
2021-12-28 18:32:55 -0300

    Random numbers working

---
+++ index.js
@@ -0,0 +1,6 @@
+// randon numbers beetween 1 and 6 included both
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+alert(randomNumber1)
+alert(randomNumber2)
